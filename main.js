b432613c91fe9f0b93ff00e76a97666e28c7afca
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2022-06-09 02:49:08 +0530

    fixes

--- main.js
+++ main.js
@@ -1,56 +1,20 @@
-import "./style.css";
 import "file-drop-element";
 import "prismjs/themes/prism-okaidia.css";
+
+import "./style.css";
+
 import Prism from "prismjs";
-import { basename, extname } from "./utils";
 
-import { registerSW } from "virtual:pwa-register";
-import { applyTemplate } from "./lib/applyTemplate";
+import {App, ImageTask} from "./lib/app";
+import {applyTemplate} from "./lib/applyTemplate";
 import {
-  getConversionStrategyForInput,
-  getMimeTypeFromStrategy,
-  getExtensionFromMimeType,
-  getCodecFromMimeType,
   getCLIOptionsFromMimeType,
+  getCodecFromMimeType,
+  getConversionStrategyForInput,
+  getExtensionFromMimeType,
+  getMimeTypeFromStrategy,
 } from "./lib/mime-utils";
-
-import { App, ImageTask } from "./lib/app";
-
-import {getCLS, getFID, getLCP} from 'web-vitals';
-
-function sendToGoogleAnalytics({name, delta, value, id}) {
-  // Assumes the global `gtag()` function exists, see:
-  // https://developers.google.com/analytics/devguides/collection/ga4
-  gtag('event', name, {
-    // Built-in params:
-    value: delta, // Use `delta` so the value can be summed.
-    // Custom params:
-    metric_id: id, // Needed to aggregate events.
-    metric_value: value, // Optional.
-    metric_delta: delta, // Optional.
-
-    // OPTIONAL: any additional params or debug info here.
-    // See: https://web.dev/debug-web-vitals-in-the-field/
-    // metric_rating: 'good' | 'ni' | 'poor',
-    // debug_info: '...',
-    // ...
-  });
-}
-
-window.addEventListener("load", () => {
-  getCLS(sendToGoogleAnalytics);
-  getFID(sendToGoogleAnalytics);
-  getLCP(sendToGoogleAnalytics);
-});
-
-const updateSW = registerSW({
-  onNeedRefresh() {
-    // show a prompt to user
-  },
-  onOfflineReady() {
-    // show a ready to work offline to user
-  },
-});
+import {basename, extname} from "./utils";
 
 let app = new App();
 
@@ -86,7 +50,7 @@ const onSaveAll = async (e) => {
 const onFile = (e) => {
   const file = (e.target?.files || e.files)[0];
 
-  const { state, optimizer } = app;
+  const {state, optimizer} = app;
 
   state.fileName = file.name;
   state.images.length = 0;
@@ -94,30 +58,29 @@ const onFile = (e) => {
 
   goButton.disabled = false;
 
-  optimizer
-    .load(file)
-    .then(() => {
+  optimizer.load(file).then(() => {
+    state.images.length = 0;
+    // create the images we need to populate.
+    for (let [width, height] of optimizer.optimalSizes) {
+      state.images.push(new ImageTask(width, height));
+    }
 
-      state.images.length = 0;
-      // create the images we need to populate.
-      for (let [width, height] of optimizer.optimalSizes) {
-        state.images.push(new ImageTask(width, height));
-      }
-
-      render();
-    });
+    render();
+  });
 };
 
 const onGoClicked = async (e) => {
-  const { optimizer, optimizer: {file}, state } = app;
+  const {
+    optimizer,
+    optimizer: {file},
+    state,
+  } = app;
 
   state.running = true;
 
   // We have to load the image to get the width and height.
-  const [
-    sourceElementStrategy,
-    imgElementStrategy,
-  ] = getConversionStrategyForInput(file.type);
+  const [sourceElementStrategy, imgElementStrategy] =
+    getConversionStrategyForInput(file.type);
 
   const sourceElementMimeType = getMimeTypeFromStrategy(sourceElementStrategy);
   const imgElementMimeType = getMimeTypeFromStrategy(imgElementStrategy);
@@ -139,20 +102,16 @@ const onBasePathChanged = (e) => {
 };
 
 const render = () => {
-  const { state, optimizer } = app;
-  const { type } = optimizer.metadata;
+  const {state, optimizer} = app;
+  const {type} = optimizer.metadata;
 
   let root = document.documentElement;
-  root.style.setProperty("--filename", `'${ state.fileName }'`);
-  
-  const [
-    sourceElementStrategy,
-    imgElementStrategy,
-  ] = getConversionStrategyForInput(type);
+  root.style.setProperty("--filename", `'${state.fileName}'`);
 
-  const sourceElementMimeType = getMimeTypeFromStrategy(
-    sourceElementStrategy
-  );
+  const [sourceElementStrategy, imgElementStrategy] =
+    getConversionStrategyForInput(type);
+
+  const sourceElementMimeType = getMimeTypeFromStrategy(sourceElementStrategy);
   const imgElementMimeType = getMimeTypeFromStrategy(imgElementStrategy);
 
   updateCLI(sourceElementStrategy, imgElementStrategy);
@@ -180,62 +139,86 @@ Prism.hooks.add("before-highlight", function (env) {
   env.code = env.element.innerText;
 });
 
-const updateHTML = (
-  sourceElementMimeType,
-  imgElementMimeType
-) => {
+const updateHTML = (sourceElementMimeType, imgElementMimeType) => {
   const sizes = app.optimizer.optimalSizes;
-  const { naturalHeight, naturalWidth } = app.optimizer.metadata;
+  const {naturalHeight, naturalWidth} = app.optimizer.metadata;
   const name = app.state.fileName;
   const extension = extname(name);
   const nameNoExtension = basename(name, extension);
 
   const sourceExtension = getExtensionFromMimeType(sourceElementMimeType);
-  const imgExtension = getExtensionFromMimeType(imgElementMimeType);
+  const imgExtension = getExtensionFromMimeType(imgElementMimeType).replace(
+    "jpeg",
+    "jpg"
+  );
 
-  let sources = `<source 
+  let imports = `
+    import orig from "./${nameNoExtension}.${imgExtension}";
+    
+    ${sizes
+      .map(([width]) =>
+        `
+    import img_${width}_${imgExtension} from "./${nameNoExtension}-${width}.${imgExtension}";    
+    import img_${width}_${sourceExtension} from "./${nameNoExtension}-${width}.${sourceExtension}";
+    `.trim()
+      )
+      .join("\n")}
+
+  `;
+  let sources = `
+  <source 
     type="${sourceElementMimeType}"
     sizes="100vw"
-    srcset="${sizes
-      .map(
-        ([width]) =>
-          `${encodeURIComponent(
-            nameNoExtension
-          )}\-${width}.${sourceExtension} ${width}w`
+    srcset={\`${sizes
+      .map(([width]) =>
+        ` 
+   \${img_${width}_${sourceExtension}}  ${width}w`.trim()
       )
-      .join(", \n\t\t")}">`;
+      .join(", \n\t\t")}
+    \`}/>`;
 
-  code.innerText = `<picture>
-  ${sources}
-  <img 
-    alt="The author should add a description of what appears in the image."
-    src="${encodeURIComponent(nameNoExtension)}.${imgExtension}" 
-    srcset="${sizes
-      .map(
-        ([width]) =>
-          `${encodeURIComponent(
-            nameNoExtension
-          )}\-${width}.${imgExtension} ${width}w`
-      )
-      .join(", \n\t\t")}"
-    sizes="100vw"
-    loading="lazy"
-    decoding="async"
-    height="${naturalHeight}"
-    width="${naturalWidth}"
-    style="content-visibility: auto; max-width: 100%; height: auto;">
-</picture>`;
+  code.innerText = `
+${imports}
+import {css} from "catom"
+
+export function I() {
+
+return   <picture>
+      ${sources}
+      <img 
+        alt="The author should add a description of what appears in the image."
+        src={orig} 
+        class={css({borderRadius:"20px"})}
+        srcset={\`${sizes
+          .map(([width]) => `\${img_${width}_${imgExtension}} ${width}w`)
+          .join(", \n\t\t")}\`}
+        sizes="100vw"
+        loading="lazy"
+        //@ts-ignore
+        decoding="async"
+        height="${naturalHeight}"
+        width="${naturalWidth}"
+        style="content-visibility: auto; max-width: 100%; height: auto;"/>
+</picture>
+        }
+`;
 };
 
 const updateCLI = (targetBaseCodec, targetSourceCodec) => {
   const sizes = app.optimizer.optimalSizes;
   const name = app.optimizer.metadata.name;
 
-  let cliCommands = sizes.map(
-    ([width, height]) =>
-      `npx @squoosh/cli --resize "{width: ${width}, height: ${height}}" --${targetSourceCodec} auto -s \-${width} ${name
-        .split(" ")
-        .join("\\ ")}`
+  let cliCommands = [
+    `npx @squoosh/cli --${targetSourceCodec} '{"quality":75}' ${name
+      .split(" ")
+      .join("\\ ")}`,
+  ].concat(
+    sizes.map(
+      ([width, height]) =>
+        `npx @squoosh/cli --resize "{width: ${width}, height: ${height}}" --${targetSourceCodec} auto -s \-${width} ${name
+          .split(" ")
+          .join("\\ ")}`
+    )
   );
 
   cliCommands.push(
@@ -246,12 +229,12 @@ const updateCLI = (targetBaseCodec, targetSourceCodec) => {
           .join("\\ ")}`
     )
   );
-  cli.innerText = cliCommands.join(" && \\ \n  ");
+  cli.innerText = cliCommands.join(" && \n  ");
 };
 
 const renderOriginal = (rootElement) => {
-  const { file, metadata } = app.optimizer;
-  const { name } = file;
+  const {file, metadata} = app.optimizer;
+  const {name} = file;
 
   rootElement.appendChild(
     applyTemplate(codecOutputTemplate, {
@@ -268,14 +251,14 @@ const renderOriginal = (rootElement) => {
       url: URL.createObjectURL(file),
       width: metadata.naturalWidth,
       height: metadata.naturalHeight,
-      status: 'original',
+      status: "original",
       size: `${(file.size / 1024).toFixed(2)} KB`,
     })
   );
 };
 
 function updateWorklistUI(rootElement, mimeType) {
-  const { fileName, images } = app.state;
+  const {fileName, images} = app.state;
   const extension = getExtensionFromMimeType(mimeType);
 
   rootElement.appendChild(
@@ -290,28 +273,30 @@ function updateWorklistUI(rootElement, mimeType) {
   );
 
   for (let image of images) {
-    const { width, height, url, size } = image;
+    const {width, height, url, size} = image;
     outputElement.appendChild(
       applyTemplate(previewListItemTemplate, {
         name: `${basename(fileName, extname(fileName))}-${width}.${extension}`,
         width,
         height,
         url,
-        status: (url) ? 'done' : 'not-started',
-        size: (size == 0) ? 'Unknown Size' : `${(size / 1024).toFixed(2)} KB`,
+        status: url ? "done" : "not-started",
+        size: size == 0 ? "Unknown Size" : `${(size / 1024).toFixed(2)} KB`,
       })
     );
   }
 }
 
 async function compressAndOutputImages(mimeType) {
-  const { optimizer, state: {images, fileName} } = app;
+  const {
+    optimizer,
+    state: {images, fileName},
+  } = app;
   const extension = getExtensionFromMimeType(mimeType);
   const cliOptions = getCLIOptionsFromMimeType(mimeType);
 
   for (let image of images) {
-
-    cliOptions["resize"] = { width:image.width, height:image.height };
+    cliOptions["resize"] = {width: image.width, height: image.height};
 
     const elementToUpdate = document.getElementById(
       `${basename(fileName, extname(fileName))}-${image.width}.${extension}`
@@ -323,14 +308,16 @@ async function compressAndOutputImages(mimeType) {
 
     for (let file of results.values()) {
       const outputFile = file.outputs[0];
-      const newName = `${basename(fileName, extname(fileName))}-${image.width}.${extension}`;
+      const newName = `${basename(fileName, extname(fileName))}-${
+        image.width
+      }.${extension}`;
       const output = new File([file.outputs[0].out.buffer], newName, {
         type: mimeType,
       });
 
       image.compressedImage = output;
       image.size = outputFile.outputSize;
-  
+
       render();
     }
   }
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { VitePWA } from 'vite-plugin-pwa'
-
+/**
+ * @type {import("vite").UserConfig}
+ */
 export default {
   build: {
-    target: 'es2020',
-    minify: 'esbuild',
-    soucemap: false
+    target: "es2020",
+    minify: "esbuild",
+    soucemap: false,
   },
-  plugins: [
-    VitePWA({
-      base: '/',
-      mode: 'development',
-    })
-  ]
-}
+  plugins: [],
+};
